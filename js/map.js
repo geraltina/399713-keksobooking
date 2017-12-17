44b0c2e0be2941b535993819aef72e9a45746e98
geraltina <evgpestr@yandex.ru>
2017-12-17 19:42:07 +0300

    Перенос цикла

--- js/map.js
+++ js/map.js
@@ -1,12 +1,6 @@
 'use strict';
 
 window.map = (function () {
-  window.data();
-
-  var adsArray = window.data();
-  for (var i = 0; i < adsArray.length; i++) {
-    window.renderPins(adsArray[i]);
-  }
-
+  window.renderPins();
   window.dragPin();
 })();
--- js/render-pins.js
+++ js/render-pins.js
@@ -1,8 +1,13 @@
 'use strict';
 
-window.renderPins = function (arrElem) {
+window.renderPins = function () {
   var fragment = document.createDocumentFragment();
+  var adsArray = window.data();
   window.general.setupListElement = document.querySelector('.map__pins');
-  fragment.appendChild(window.renderPin(arrElem));
+
+  for (var i = 0; i < adsArray.length; i++) {
+    fragment.appendChild(window.renderPin(adsArray[i]));
+  }
+
   window.general.setupListElement.appendChild(fragment); // inserts pins in markup
 };
