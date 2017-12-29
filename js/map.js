c18545bca93864c8b67b2197fa75fcde79a306de
geraltina <evgpestr@yandex.ru>
2017-12-29 12:24:18 +0300

    Фильтры

--- js/map.js
+++ js/map.js
@@ -4,28 +4,92 @@ window.map = (function () {
   window.general.setupListElement = document.querySelector('.map__pins');
   var ads = [];
   var filter = document.querySelector('.map__filters');
-  var filterHouseType = filter.querySelector('select[name="housing-type"]');
-  var filterHousePrice = filter.querySelector('select[name="housing-price"]');
-  var filterHouseRooms = filter.querySelector('select[name="housing-rooms"]');
-  var filterHouseGuests = filter.querySelector('select[name="housing-guests"]');
+  var filterHouseType = filter.querySelector('#housing-type');
+  var filterHousePrice = filter.querySelector('#housing-price');
+  var filterHouseRooms = filter.querySelector('#housing-rooms');
+  var filterHouseGuests = filter.querySelector('#housing-guests');
   var filterHouseFeatures = filter.querySelector('#housing-features');
+  var featureCheckboxes = filterHouseFeatures.querySelectorAll('input[type="checkbox"]');
   var setAdsFilter = function () {
-    var getHouseType = ads.filter(function (it) {
-      return it.offer.type === filterHouseType.value;
+    var check = function (it) {
+      var isAny = function (filterValue, offerValue) {
+        if (filterValue !== 'any') {
+          if (offerValue !== +(filterValue)) {
+            return false;
+          } else {
+            return true;
+          }
+        } else {
+          return true;
+        }
+      };
+
+      isAny(filterHouseType.value, it.offer.type); // checks type of house
+      isAny(filterHouseRooms.value, it.offer.rooms); // checks number of rooms in the house
+      isAny(filterHouseGuests.value, it.offer.guests); // checks number of guests in the house
+
+      switch (filterHousePrice.value) { // checks price of the house
+        case 'low':
+          if (it.offer.price > 10000) {
+            return false;
+          }
+          break;
+        case 'middle':
+          if (it.offer.price < 10000 && it.offer.price > 50000) {
+            return false;
+          }
+          break;
+        case 'high':
+          if (it.offer.price < 50000) {
+            return false;
+          }
+          break;
+        default:
+          return true;
+      }
+
+      for (var x = 0; x < featureCheckboxes.length; x++) { // checks features
+        if (featureCheckboxes[x].checked) {
+          if (it.offer.features[x] !== featureCheckboxes[x]) {
+            return false;
+          } else {
+            return true;
+          }
+        } else {
+          return true;
+        }
+      }
+
+      return true;
+    };
+
+    var checkedOffers = ads.filter(check);
+    var uniquePins = checkedOffers.filter(function (it, i) {
+      return checkedOffers.indexOf(it) === i;
     });
-    var filteredPins = getHouseType;
-    var uniquePins = filteredPins.filter(function (it, i) {
-      return filteredPins.indexOf(it) === i;
-    });
-    window.render(uniquePins);
+    var filteredOffers = uniquePins.slice(0, 5);
+
+    window.render(filteredOffers);
   };
 
-  var changeTypeFilter = filterHouseType.addEventListener('change', function () {
-    setAdsFilter();
-  });
+  var removePins = function () {
+    var pins = document.querySelectorAll('.map__pin');
+    for (var j = 0; j < pins.length; j++) {
+      if (!pins[j].classList.contains('map__pin--main')) {
+        pins[j].remove();
+      }
+    }
+  };
 
   var successHandler = function (pins) {
     ads = pins;
+    var mapFilters = document.querySelectorAll('.map__filter');
+    for (var x = 0; x < mapFilters.length; x++) {
+      mapFilters[x].addEventListener('change', function () {
+        removePins();
+        setAdsFilter();
+      });
+    }
     setAdsFilter();
   };
 
--- js/render.js
+++ js/render.js
@@ -4,11 +4,7 @@ window.render = function (pins) {
   var fragment = document.createDocumentFragment();
   var adsNumber = pins.length > 5 ? 5 : pins.length;
   for (var i = 0; i < adsNumber; i++) {
-    if (fragment.firstChild) {
-      fragment.removeChild(window.renderPin(pins[i]));
-    } else {
-      fragment.appendChild(window.renderPin(pins[i]));
-    }
+    fragment.appendChild(window.renderPin(pins[i]));
   }
   window.general.setupListElement.appendChild(fragment); // inserts pins in markup
 };
